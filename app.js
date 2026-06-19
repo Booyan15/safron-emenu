20ffe53633cb1599bf3527b0388eab07761bd76b
Bojan Popovski <popovskibojan15@gmail.com>
2026-06-19 18:53:22 +0200

    Update shakshuka image path

--- app.js
+++ app.js
@@ -286,7 +286,7 @@ const menuProducts = [
     name: "Прженица со јајца (шакшука)",
     category: "Доручек",
     price: 180,
-    image: "/optimized/menu-images/шакшука.jpg"
+    image: "/menu-images/шакшука.jpg"
   },
   {
     id: "vrap",
